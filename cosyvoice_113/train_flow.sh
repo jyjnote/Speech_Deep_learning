f62689a7630948e5db6e0241058fe2ffa38246bd
Jaeyeon <144209498+jyjnote@users.noreply.github.com>
2025-08-11 20:13:07 +0900

    Update train_flow.sh

--- cosyvoice_113/train_flow.sh
+++ cosyvoice_113/train_flow.sh
@@ -1,3 +1,4 @@
+PYTHONPATH=$PYTHONPATH:/mnt/raid0/jjy/CosyVoice/third_party/Matcha-TTS \
 CUDA_VISIBLE_DEVICES=4 torchrun --nnodes=1 --nproc_per_node=1 \
   --rdzv_id=ljs_flow --rdzv_backend=c10d --rdzv_endpoint=localhost:1234 \
   cosyvoice/bin/train.py \
@@ -6,6 +7,7 @@ CUDA_VISIBLE_DEVICES=4 torchrun --nnodes=1 --nproc_per_node=1 \
   --train_data data/train.data.list \
   --cv_data data/dev.data.list \
   --model flow \
+  --checkpoint /mnt/raid0/jjy/CosyVoice/exp/ljspeech/flow/epoch_20_whole.pt \
   --model_dir exp/ljspeech/flow \
   --tensorboard_dir tensorboard/ljspeech/flow \
   --ddp.dist_backend nccl \
